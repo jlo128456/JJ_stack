0f7ef895b58e4c3bb9a18ac54d0dfa27b2b92e66
Jeffrey Lo <jlo128456@gmail.com>
2026-04-12 20:14:14 +1000

    updated app.js- 12-04-2026

--- jj__stack/src/App.js
+++ jj__stack/src/App.js
@@ -1,2 +1,7 @@
 import { useState, useEffect, useRef } from "react";
-imort GlobalStyles form 
+import GlobalStyles from "./components/GlobalStyles";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Services from "./components/Servies";
+import Work from "./components/Work";
+import ContactForm from "./components/ContactForm"
