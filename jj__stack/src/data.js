87caeee359d7bf9339e38e2676f26342aa93acd4
Jeffrey Lo <jlo128456@gmail.com>
2026-04-13 21:31:32 +1000

    updated data.js 13-04-2026

--- jj__stack/src/data.js
+++ jj__stack/src/data.js
@@ -10,6 +10,6 @@ export const services = [
 
 export const projects = [
   { name: "Web app", type: "Job Management CRM", url: "https://padigital.com.au", image: "/pa-digital.png" },
-  { name: "Prefect Agencies", type: "Python CRM — Work orders, customers, inventory & invoicing.", url: null, image: "/pa-crm.png" },
+  { name: "Prefect Agencies", type: "Python CRM — Work orders, customers, inventory & invoicing.", url: "/pa-crm.png", image: "/pa-crm.png" },
   { name: "JJ Stack", type: "Company Website", url: "https://jjstack.com.au", image: "/jj-stak.png" },
 ];
